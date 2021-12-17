0765503c497689bc9e7b2cd90a37d31df5350a02
max <50576299+Pasitha@users.noreply.github.com>
2021-12-17 23:45:28 +0700

    prepare main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,108 @@
-#include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
-#include <iostream>
-#include <sstream>
-#include <chrono>
-#include <cstdlib>
-#include <functional>
-#include "Button.h"
+enum eScene {
+	menu, game, pause
+};
 
+sf::Texture createTexture(std::string path) {
+	sf::Texture texture;
+	texture.loadFromFile(path);
+
+	return texture;
+}
+
+#define NEW
+#ifdef NEW
+
+sf::RenderWindow window(sf::VideoMode(1920, 1080), "JukkyJung Adventure", sf::Style::Fullscreen);
+
+int main() {
+	unsigned short int scene = game;
+
+	sf::Image icon;
+	icon.loadFromFile("picture/Jukubot_FA2.png");
+	window.setIcon(1800, 1800, icon.getPixelsPtr());
+	window.setFramerateLimit(60);
+
+	sf::RectangleShape background(sf::Vector2f(1920.f, 1080.f));
+	sf::Texture backgroundTexture = createTexture("picture/Jukkyjung_adventure_background1.png");
+
+	background.setTexture(&backgroundTexture);
+
+	pasitha::sfml::Button b_NextState("Next State", "picture/button.png", sf::Vector2f(1500.f, 880.f));
+
+	// main loop
+	while (window.isOpen()) {
+		// Event loop
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			switch (event.type) {
+			case sf::Event::Closed:
+				window.close();
+				break;
+//			case sf::Event::MouseMoved:
+//				if (isHover(Inventory_button)) {
+//					Inventory_button.setFillColor(sf::Color(155, 155, 155, 255));
+//				}
+//				else {
+//					Inventory_button.setFillColor(sf::Color(255, 255, 255, 255));
+//				}
+//
+//				if (isHover(NextState_button)) {
+//					NextState_button.setFillColor(sf::Color(155, 155, 155, 255));
+//				}
+//				else {
+//					NextState_button.setFillColor(sf::Color(255, 255, 255, 255));
+//				}
+//				break;
+//			case sf::Event::MouseButtonPressed:
+//				if (isHover(Inventory_button)) {
+//					std::cout << "If you want to play the full game, you can wait to follow on Github." << std::endl << "aHR0cHM6Ly9naXRodWIuY29tL1Bhc2l0aGEvSnVra3lKdW5nLUFkdmVudHVyZQ==";
+//				}
+//				if (isHover(NextState_button)) {
+//
+//					stateEvent = (rand() % 100) + 1;
+//
+//					state.setString("State " + std::to_string(++stateI));
+//
+//					backgroundTexture.loadFromFile("picture/Jukkyjung_adventure_background" + std::to_string((rand() % 2) + 1) + ".png");
+//					background.setTexture(&backgroundTexture);
+//				}
+//				break;
+			}
+		}
+
+		// display
+		window.clear();
+
+		switch (scene) {
+		case menu:
+			
+			break;
+		case game:
+			window.draw(background);
+			b_NextState.draw(&window);
+
+			break;
+		}
+
+		window.display();
+
+	}
+
+}
+#else
 int main() {
 	int stateI = 1;
 	int stateEvent = 0;
 
-	sf::Music music;
-	music.openFromFile("sound/Mighty and Meek - Kevin MacLeod.wav");
-	music.setVolume(20.f);
-	music.play();
-	music.setLoop(true);
+	sf::Music music[2];
+	music[0].openFromFile("sound/Mighty and Meek - Kevin MacLeod.wav");
+	music[0].setVolume(20.f);
+	music[0].play();
+	music[0].setLoop(true);
+
+	music[1].openFromFile("sound/March of the Mind - Kevin MacLeod.wav");
+	music[1].setVolume(20.f);
+	music[1].setLoop(true);
 
 	sf::Font ReadexPro;
 	ReadexPro.loadFromFile("font/ReadexPro.ttf");
@@ -35,7 +122,7 @@ int main() {
 		text.setStyle(sf::Text::Bold);
 	};
 
-	auto isHover = [&window](sf::RectangleShape button) -> bool {
+	auto isHover = [&window](sf::RectangleShape& button) -> bool {
 		float mouseX = sf::Mouse::getPosition(window).x;
 		float mouseY = sf::Mouse::getPosition(window).y;
 
@@ -44,11 +131,11 @@ int main() {
 
 		float btnxPosWidth = btnPosX + button.getLocalBounds().width;
 		float btnyPosHeight = btnPosY + button.getLocalBounds().height;
-		
-		return mouseX < btnxPosWidth && mouseX > btnPosX && mouseY < btnyPosHeight && mouseY > btnPosY;
+
+		return mouseX < btnxPosWidth&& mouseX > btnPosX && mouseY < btnyPosHeight&& mouseY > btnPosY;
 	};
 
-	auto createButton = [&ReadexPro](sf::RectangleShape &button, sf::Texture &buttonTexture, sf::Text &buttonLable, sf::Vector2f position) -> void {
+	auto createButton = [&ReadexPro](sf::RectangleShape& button, sf::Texture& buttonTexture, sf::Text& buttonLable, sf::Vector2f position) -> void {
 		button.setPosition(position);
 		button.setTexture(&buttonTexture);
 
@@ -56,6 +143,12 @@ int main() {
 		buttonLable.setStyle(sf::Text::Bold);
 		buttonLable.setFillColor(sf::Color::Black);
 		buttonLable.setPosition({ button.getPosition().x + button.getGlobalBounds().width / 2.f - buttonLable.getGlobalBounds().width / 2.f, button.getPosition().y + button.getGlobalBounds().height / 2.2f - buttonLable.getGlobalBounds().height / 2.2f });
+	};
+
+	auto createTexture = [](std::string path) -> sf::Texture {
+		sf::Texture texture;
+		texture.loadFromFile(path);
+		return texture;
 	};
 
 	// game display
@@ -71,8 +164,7 @@ int main() {
 	// monster
 	sf::RectangleShape monster(sf::Vector2f(318.f, 415.f));
 
-
-	sf::Texture backgroundTexture;
+	sf::Texture backgroundTexture = createTexture("picture/Jukkyjung_adventure_background1.png");
 	sf::Texture jukkyjungTexture;
 	sf::Texture monsterTexture;
 	sf::Texture buttonTexture;
@@ -82,9 +174,9 @@ int main() {
 	monster.setPosition(1480.f, 460.f);
 
 	monsterTexture.loadFromFile("picture/truefaster.png");
-	backgroundTexture.loadFromFile("picture/Jukkyjung_adventure_background1.png");
+	// backgroundTexture.loadFromFile("picture/Jukkyjung_adventure_background1.png");
 	jukkyjungTexture.loadFromFile("picture/Jukubot_FA2.png");
-	
+
 	buttonTexture.loadFromFile("picture/button.png");
 
 	sf::Text nextstate("Next State", ReadexPro);
@@ -92,15 +184,17 @@ int main() {
 	createButton(NextState_button, buttonTexture, nextstate, sf::Vector2f(1500.f, 880.f));
 	createButton(Inventory_button, buttonTexture, inventory, sf::Vector2f(75.f, 880.f));
 
+	pasitha::sfml::Button nextStateButton("Next State", "picture/button.png", sf::Vector2f(1500.f, 880.f));
+
 	background.setTexture(&backgroundTexture);
 	JukkyJung.setTexture(&jukkyjungTexture);
 	monster.setTexture(&monsterTexture);
-	
+
 	sf::Text state("STATE 1", ReadexPro);
 	sf::Text Hp("HP: 10", ReadexPro);
 	sf::Text Atk("ATK: 1", ReadexPro);
 	sf::Text Defence("DEF: 1", ReadexPro);
-		
+
 	setTextBold(state, 48, sf::Vector2f(48, 50));
 	setTextBold(Hp, 36, sf::Vector2f(1750.f, 50.f));
 	setTextBold(Atk, 36, sf::Vector2f(1750.f, 85.f));
@@ -135,7 +229,9 @@ int main() {
 					std::cout << "If you want to play the full game, you can wait to follow on Github." << std::endl << "aHR0cHM6Ly9naXRodWIuY29tL1Bhc2l0aGEvSnVra3lKdW5nLUFkdmVudHVyZQ==";
 				}
 				if (isHover(NextState_button)) {
+
 					stateEvent = (rand() % 100) + 1;
+
 					state.setString("State " + std::to_string(++stateI));
 
 					backgroundTexture.loadFromFile("picture/Jukkyjung_adventure_background" + std::to_string((rand() % 2) + 1) + ".png");
@@ -144,7 +240,7 @@ int main() {
 				break;
 			}
 		}
-		
+
 		window.clear();
 		// picture
 		window.draw(background);
@@ -165,15 +261,17 @@ int main() {
 		window.draw(Hp);
 		window.draw(Atk);
 		window.draw(Defence);
-		
+
 		window.draw(Inventory_button);
 		window.draw(inventory);
-		
+
 		// button
 		window.draw(NextState_button);
 		window.draw(nextstate);
+		// nextStateButton.draw(window);
 		window.display();
 	}
-
 	return 0;
 }
+
+#endif OLD
