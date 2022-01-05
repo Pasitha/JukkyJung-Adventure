49ad2503a5841c529787e075c2a3ad1b76aeadcf
max <50576299+Pasitha@users.noreply.github.com>
2022-01-05 19:48:18 +0700

    add new commond include file for pasitha.h

---
+++ common.h
@@ -0,0 +1,11 @@
+#pragma once
+// STL
+#include <iostream>
+#include <chrono>
+#include <sstream>
+#include <cstdlib>
+#include <functional>
+
+// SFML
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
