e8117a45f470c2ef6e666b39b1b65f32f83d72a3
Pasitha <pasithacppsom@hotmail.com>
2022-05-17 23:31:00 +0700

    test sfml

--- JukkyJungAdventure/JukkyJungAdventure/main.cpp
+++ JukkyJungAdventure/JukkyJungAdventure/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main() {
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML Works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+
+			window.clear();
+			window.draw(shape);
+			window.display();
+		}
+	}
+
 	std::cout << "Hello World" << std::endl;
 	return 0;
 }
