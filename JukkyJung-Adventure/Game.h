73142296561164d1839039969d14707d9103f393
Pasitha <pasithacppsom@hotmail.com>
2022-10-29 13:07:47 +0700

    add Game object

--- JukkyJung-Adventure/Game.h
+++ JukkyJung-Adventure/Game.h
@@ -1,6 +1,15 @@
 #pragma once
 #include "common.h"
 
+class Game {
+public:
+	Game();
+
+	void Update();
+private:
+	sf::RenderWindow m_window;
+};
+
 auto render = [](sf::RenderWindow* window) -> void {
 	window->setActive(true);
 	sf::CircleShape shape(100.f);
