114e0f3e42c565abba5a9ce926a23ee0f3a748d3
Pasitha <pasithacppsom@hotmail.com>
2022-11-01 21:27:51 +0700

    setup sprite

--- JukkyJung-Adventure/JukkyJung.cpp
+++ JukkyJung-Adventure/JukkyJung.cpp
@@ -4,6 +4,8 @@ JukkyJung::JukkyJung(sf::RenderWindow* window) : m_windowInstance(window), m_Juk
 	FileManager::LoadFormFile(this->m_JukkyJungTexture, "asset/picture/JukkyJung.png");
 	
 	this->m_JukkyJungSprite.setTexture(m_JukkyJungTexture);
+	this->m_JukkyJungSprite.setScale({ 0.3, 0.3 });
+	this->m_JukkyJungSprite.setPosition({ 30, 30 });
 }
 
 void JukkyJung::Update() {
