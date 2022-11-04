cbecd38382e98eac78ada363512561116b62c8d5
Pasitha <pasithacppsom@hotmail.com>
2022-11-04 21:36:19 +0700

    update class method

--- JukkyJung-Adventure/Button.cpp
+++ JukkyJung-Adventure/Button.cpp
@@ -1,11 +1,20 @@
 #include "Button.h"
 
 Button::Button(sf::RenderWindow* window) : m_windowInstance(window) {
-	FileManager::LoadFormFile(this->m_ButtonTexture, "asset/picture/JukkyJung.png");
+	FileManager::LoadFormFile(this->m_ButtonTexture, "asset/picture/Button.png");
 
 	m_ButtonSprite.setTexture(this->m_ButtonTexture);
 }
 
+void Button::addButton() {
+	this->m_Button.push_back(this->m_ButtonSprite);
+}
+
+void Button::setPosition(unsigned short index, sf::Vector2f position) {
+	this->m_Button[index].setPosition(position);
+}
+
 void Button::Update() {
-	this->m_windowInstance->draw(this->m_ButtonSprite);
+	for (sf::Sprite i : m_Button)
+		this->m_windowInstance->draw(i);
 }
