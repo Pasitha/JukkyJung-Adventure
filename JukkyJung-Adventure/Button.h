1a4263e0910265feb6241f68077b02c7065d6a61
Pasitha <pasithacppsom@hotmail.com>
2022-11-05 03:32:21 +0700

    add font and button lable

--- JukkyJung-Adventure/Button.h
+++ JukkyJung-Adventure/Button.h
@@ -5,16 +5,18 @@ class Button {
 public:
 	Button(sf::RenderWindow* window);
 
-	void addButton();
+	void addButton(const std::string buttonLable);
 	void setPosition(unsigned short index, sf::Vector2f position);
 	void isHover();
 	void Update();
 private:
 	sf::RenderWindow* m_windowInstance;
 
+	sf::Font m_ButtonFont;
 	sf::Texture m_ButtonTexture;
 	sf::Sprite m_ButtonSprite;
-	sf::Text m_ButtonLable;
+	sf::Text m_ButtonText;
 
 	std::vector<sf::Sprite> m_Button;
+	std::vector<sf::Text> m_ButtonLable;
 };
