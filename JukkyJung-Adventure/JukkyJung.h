e3b1b5197f5cc9cc5917f09d1a9f09c75afef73c
Pasitha <pasithacppsom@hotmail.com>
2022-11-06 23:32:26 +0700

    add text instance for display Hp

--- JukkyJung-Adventure/JukkyJung.h
+++ JukkyJung-Adventure/JukkyJung.h
@@ -9,7 +9,10 @@ public:
 private:
 	sf::RenderWindow* m_windowInstance;
 
+	sf::Font m_TextFont;
+
 	int m_JukkyJungHealth;
+	sf::Text m_JukkyJungHp;
 	sf::Texture m_JukkyJungTexture;
 	sf::Sprite m_JukkyJungSprite;
 };
