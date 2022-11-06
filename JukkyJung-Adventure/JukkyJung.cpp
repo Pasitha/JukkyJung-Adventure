0f0948a9efd213b1976b57c7121413ff1e64d8b6
Pasitha <pasithacppsom@hotmail.com>
2022-11-06 23:32:51 +0700

    update class constructor

--- JukkyJung-Adventure/JukkyJung.cpp
+++ JukkyJung-Adventure/JukkyJung.cpp
@@ -1,8 +1,12 @@
 #include "JukkyJung.h"
 
 JukkyJung::JukkyJung(sf::RenderWindow* window) : m_windowInstance(window), m_JukkyJungHealth(100) {
+	FileManager::LoadFormFile(this->m_TextFont, "asset/font/ReadexPro.ttf");
 	FileManager::LoadFormFile(this->m_JukkyJungTexture, "asset/picture/JukkyJung.png");
 	
+	m_JukkyJungHp.setFillColor(sf::Color(0, 0, 0));
+	m_JukkyJungHp.setFont(this->m_TextFont);
+
 	this->m_JukkyJungSprite.setTexture(m_JukkyJungTexture);
 	this->m_JukkyJungSprite.setScale({ 0.3f, 0.3f });
 	this->m_JukkyJungSprite.setPosition({ 30, 30 });
