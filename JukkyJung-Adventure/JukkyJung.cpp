f129daf1909ba64f2d856f900398f4362cc9448f
Pasitha <pasithacppsom@hotmail.com>
2022-11-07 23:31:48 +0700

    set string text and draw text

--- JukkyJung-Adventure/JukkyJung.cpp
+++ JukkyJung-Adventure/JukkyJung.cpp
@@ -4,8 +4,9 @@ JukkyJung::JukkyJung(sf::RenderWindow* window) : m_windowInstance(window), m_Juk
 	FileManager::LoadFormFile(this->m_TextFont, "asset/font/ReadexPro.ttf");
 	FileManager::LoadFormFile(this->m_JukkyJungTexture, "asset/picture/JukkyJung.png");
 	
-	m_JukkyJungHp.setFillColor(sf::Color(0, 0, 0));
+	m_JukkyJungHp.setFillColor(sf::Color(255, 255, 255));
 	m_JukkyJungHp.setFont(this->m_TextFont);
+	m_JukkyJungHp.setString("HP: 100");
 
 	this->m_JukkyJungSprite.setTexture(m_JukkyJungTexture);
 	this->m_JukkyJungSprite.setScale({ 0.3f, 0.3f });
@@ -13,5 +14,6 @@ JukkyJung::JukkyJung(sf::RenderWindow* window) : m_windowInstance(window), m_Juk
 }
 
 void JukkyJung::Update() {
+	this->m_windowInstance->draw(m_JukkyJungHp);
 	this->m_windowInstance->draw(m_JukkyJungSprite);
 }
