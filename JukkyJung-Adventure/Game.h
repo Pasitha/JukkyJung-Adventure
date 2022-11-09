2e22e08f2349bb2a53294a4b3930421338656466
Pasitha <pasithacppsom@hotmail.com>
2022-11-09 21:28:56 +0700

    add scene enum class

--- JukkyJung-Adventure/Game.h
+++ JukkyJung-Adventure/Game.h
@@ -11,6 +11,13 @@ public:
 	void Update();
 private:
 	sf::RenderWindow m_window;
+
+	enum class m_Scene {
+		mainMenu,
+		setting,
+		gamePlay
+	};
+
 	Button* m_Button;
 	JukkyJung* m_JukkyJung;
 };
