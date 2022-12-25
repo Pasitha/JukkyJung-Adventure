4e45747a3330111eb80caa312ba1d0ce7abf52fc
Pasitha <pasithacppsom@hotmail.com>
2022-12-26 00:14:19 +0700

    update mainmenu button and some logic

--- JukkyJung-Adventure/Game.cpp
+++ JukkyJung-Adventure/Game.cpp
@@ -4,14 +4,14 @@ Game::Game() : m_window(sf::VideoMode(1920, 1080), "JukkyJuung Adventure", sf::S
 	this->m_window.setFramerateLimit(60);
 
 	this->m_SceneComponent[m_Scene::mainMenu] = {
-		new Button(&this->m_window),
-		new JukkyJung(&this->m_window)
-	};
-
-	this->m_SceneComponent[m_Scene::gamePlay] = {
 		new Button(&this->m_window)
 	};
 
+	this->m_SceneComponent[m_Scene::gamePlay] = {
+		new Button(&this->m_window),
+		new JukkyJung(&this->m_window)
+	};
+	
 	this->m_SceneComponent[m_Scene::mainMenu].m_Button->addButton("Play");
 	this->m_SceneComponent[m_Scene::mainMenu].m_Button->addButton("Setting");
 	this->m_SceneComponent[m_Scene::mainMenu].m_Button->addButton("Exit");
@@ -19,9 +19,9 @@ Game::Game() : m_window(sf::VideoMode(1920, 1080), "JukkyJuung Adventure", sf::S
 	this->m_SceneComponent[m_Scene::mainMenu].m_Button->setPosition(1, { 50, 500 });
 	this->m_SceneComponent[m_Scene::mainMenu].m_Button->setPosition(2, { 50, 700 });
 
-	this->m_SceneComponent[m_Scene::gamePlay].m_Button->addButton("Test Button 1");
-	this->m_SceneComponent[m_Scene::gamePlay].m_Button->addButton("Test Button 2");
-	this->m_SceneComponent[m_Scene::gamePlay].m_Button->addButton("Test Button 3");
+	this->m_SceneComponent[m_Scene::gamePlay].m_Button->addButton("ATTACK");
+	this->m_SceneComponent[m_Scene::gamePlay].m_Button->addButton("ITEM");
+	this->m_SceneComponent[m_Scene::gamePlay].m_Button->addButton("SKIP ROUND");
 	this->m_SceneComponent[m_Scene::gamePlay].m_Button->setPosition(0, { 50, 800 });
 	this->m_SceneComponent[m_Scene::gamePlay].m_Button->setPosition(1, { 400, 800 });
 	this->m_SceneComponent[m_Scene::gamePlay].m_Button->setPosition(2, { 750, 800 });
@@ -39,11 +39,14 @@ void Game::Update() {
 			}
 			if (event.type == sf::Event::MouseButtonPressed) {
 				switch (this->m_SceneComponent[m_Scene::mainMenu].m_Button->whichButtonHover()) {
-				case (short)m_Scene::mainMenu:
+				case 0:
 					this->m_gameScene = m_Scene::gamePlay;
 					break;
-				case (short)m_Scene::gamePlay:
+				case 1:
 					this->m_gameScene = m_Scene::setting;
+					break;
+				case 2:
+					this->m_window.close();
 					break;
 				}
 			}
