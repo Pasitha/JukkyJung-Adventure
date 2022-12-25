bde745c4912ad3a037ab1b00e3188b13da9efc88
Pasitha <pasithacppsom@hotmail.com>
2022-12-26 01:32:52 +0700

    add some element and method

--- JukkyJung-Adventure/Enemy.h
+++ JukkyJung-Adventure/Enemy.h
@@ -1,8 +1,18 @@
 #pragma once
+#include "common.h"
+
 class Enemy {
 public:
+	Enemy(sf::RenderWindow* window);
 
+	void Update();
 private:
+	sf::RenderWindow* m_windowInstance;
 
+	int m_EnemyHealth;
+	sf::Font m_TextFont;
+	sf::Text m_EnemyHp;
+
+	sf::Texture m_EnemyTexture;
+	sf::Sprite m_EnemySprite;
 };
-
