9dd81bf43e0df9db1dc5bbe2cf8a7a695bc0f3cf
Pasitha <pasithacppsom@hotmail.com>
2023-11-12 18:35:10 +0700

    add some comment

--- JukkyJung-Adventure/Game.h
+++ JukkyJung-Adventure/Game.h
@@ -1,12 +1,15 @@
 #pragma once
 #include "common.h"
 
+// Forward declarations for classes used in Game
 class Button;
 class JukkyJung;
 class Enemy;
 
+// Game class is responsible for managing the game loop, handling events, and rendering scenes.
 class Game {
 private:
+	// Enumeration representing different scenes in the game
 	enum class Scene {
 		mainMenu,
 		setting,
@@ -14,6 +17,7 @@ private:
 		pauseMenu
 	};
 
+	// Struct representing game components for each scene such that Button, JukkyJug character and Enemy character component
 	struct m_gameComponent {
 		std::unique_ptr<Button> m_Button;
 		std::unique_ptr<JukkyJung> m_JukkyJung;
@@ -23,21 +27,25 @@ private:
 public:
 	Game();
 
+	// Function to handle user input events
 	void HandleEvents();
+	// Function to render the current state of the game
 	void Render();
+	// Main game loop function
 	void GameLoop();
 
 private:
-	sf::RenderWindow m_window;
+	sf::RenderWindow m_window;                  // SFML window for rendering
 
-	Scene m_gameScene;
-	std::unordered_map<Scene, std::unique_ptr<m_gameComponent>> m_SceneComponent;
+    Scene m_gameScene;                          // Current scene in the game
+    std::unordered_map<Scene, std::unique_ptr<m_gameComponent>> m_SceneComponent;  // Map of scenes to their components
 
-	sf::Font m_gameFont;
+    sf::Font m_gameFont;                        // Font used in the game for text rendering
 
-	bool m_isGamePause;
-	sf::Text m_backgroundPauseMenuText;
-	sf::RectangleShape m_backgroundPauseMenu;
+    bool m_isGamePause;                         // Flag indicating whether the game is in a paused state
+    sf::Text m_backgroundPauseMenuText;         // Text displayed in the background during pause
+    sf::RectangleShape m_backgroundPauseMenu;   // Background shape during pause
 
-	void initializeSceneComponent(Scene scene, const std::vector<std::pair<std::string, sf::Vector2f>>& buttons);
+    // Function to initialize components for a specific scene with buttons and positions
+    void initializeSceneComponent(Scene scene, const std::vector<std::pair<std::string, sf::Vector2f>>& buttons);
 };
