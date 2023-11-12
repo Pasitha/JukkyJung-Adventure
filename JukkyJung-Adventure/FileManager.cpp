51de00ac8817f6268278abcb6462d485bd3a7589
Pasitha <pasithacppsom@hotmail.com>
2023-11-12 18:29:33 +0700

    update FileManager add file cache for resource

--- JukkyJung-Adventure/FileManager.cpp
+++ JukkyJung-Adventure/FileManager.cpp
@@ -1,36 +1,92 @@
 #include "FileManager.h"
 
+// Initialize static variables
 std::vector<std::string> FileManager::missingFiles;
+std::unordered_map<std::string, sf::Image> FileManager::imageCache;
+std::unordered_map<std::string, sf::Texture> FileManager::textureCache;
+std::unordered_map<std::string, sf::Font> FileManager::fontCache;
+std::unordered_map<std::string, sf::SoundBuffer> FileManager::soundBufferCache;
+std::unordered_map<std::string, sf::Music> FileManager::musicCache;
 
-template<typename T> void FileManager::LoadFormFile(T& t, const std::string& fileName) {
-	if (!t.loadFromFile(fileName)) {
-		missingFiles.push_back(fileName);
-	}
+// Template function to get the cache based on resource type
+template <typename T>
+std::unordered_map<std::string, T>& FileManager::GetCache() {
+    if constexpr (std::is_same_v<T, sf::Image>) {
+        return imageCache;
+    }
+    else if constexpr (std::is_same_v<T, sf::Texture>) {
+        return textureCache;
+    }
+    else if constexpr (std::is_same_v<T, sf::Font>) {
+        return fontCache;
+    }
+    else if constexpr (std::is_same_v<T, sf::SoundBuffer>) {
+        return soundBufferCache;
+    }
+    else if constexpr (std::is_same_v<T, sf::Music>) {
+        return musicCache;
+    }
+    else {
+        // Handle unsupported types
+        static_assert(always_false<T>::value, "Unsupported resource type");
+    }
 }
 
-template void FileManager::LoadFormFile<sf::Image>(sf::Image& t, const std::string& fileName);
-template void FileManager::LoadFormFile<sf::Texture>(sf::Texture& t, const std::string& fileName);
-template void FileManager::LoadFormFile<sf::Font>(sf::Font& t, const std::string& fileName);
-template void FileManager::LoadFormFile<sf::SoundBuffer>(sf::SoundBuffer& t, const std::string& fileName);
+// Template function to load a resource from a file
+template <typename T> void FileManager::LoadFromFile(T& resource, const std::string& fileName) {
+    // Check if the resource is already in the cache
+    auto& cache = GetCache<T>();
 
-template<> void  FileManager::LoadFormFile<sf::Music>(sf::Music& t, const std::string& fileName) {
-	if (!t.openFromFile(fileName)) {
-		missingFiles.push_back(fileName);
-	}
+    auto it = cache.find(fileName);
+    if (it != cache.end()) {
+        resource = it->second;
+    }
+    else {
+        // Load the resource from file
+        if (!resource.loadFromFile(fileName)) {
+            LogLoadingFailure(fileName);
+        }
+        else {
+            // If loading is successful, add it to the cache
+            cache[fileName] = resource;
+        }
+    }
 }
 
+// Explicit template instantiations for different resource types
+template void FileManager::LoadFromFile<sf::Image>(sf::Image& resource, const std::string& fileName);
+template void FileManager::LoadFromFile<sf::Texture>(sf::Texture& resource, const std::string& fileName);
+template void FileManager::LoadFromFile<sf::Font>(sf::Font& resource, const std::string& fileName);
+template void FileManager::LoadFromFile<sf::SoundBuffer>(sf::SoundBuffer& resource, const std::string& fileName);
+
+// Specialization for sf::Music
+template<> void FileManager::LoadFromFile<sf::Music>(sf::Music& resource, const std::string& fileName) {
+    // Use openFromFile for sf::Music
+    if (!resource.openFromFile(fileName)) {
+        LogLoadingFailure(fileName);
+    }
+}
+
+// Function to get a list of missing files
 bool FileManager::getMissingFileList(std::string& fileList) {
-	if (missingFiles.empty()) {
-		return false;
-	}
-	else {
-		for (auto it : missingFiles) {
-			fileList += it + '\n';
-		}
-		return true;
-	}
+    if (missingFiles.empty()) {
+        return false;
+    }
+    else {
+        for (auto it : missingFiles) {
+            fileList += it + '\n';
+        }
+        return true;
+    }
 }
 
+// Function to check if any file is missing
 bool FileManager::IsAnyFileMissing() {
-	return !missingFiles.empty();
+    return !missingFiles.empty();
+}
+
+// Function to log a loading failure with the filename
+void FileManager::LogLoadingFailure(const std::string& fileName) {
+    std::cerr << "Failed to load file: " << fileName << std::endl;
+    missingFiles.push_back(fileName);
 }
--- JukkyJung-Adventure/FileManager.h
+++ JukkyJung-Adventure/FileManager.h
@@ -1,14 +1,43 @@
 #pragma once
 #include "common.h"
 
+// Define a struct to always evaluate to false, used for static assertions
+template <class T>
+struct always_false : std::false_type {};
+
+// FileManager class for handling resource loading and caching
 class FileManager {
 private:
-	FileManager() {}
-	static std::vector<std::string> missingFiles;
+    FileManager() {}
+
+    // Template function to get the cache based on resource type
+    template <typename T> static std::unordered_map<std::string, T>& GetCache();
+
+    // Vector to store missing files during resource loading
+    static std::vector<std::string> missingFiles;
+
+    // Caches for different resource types
+    static std::unordered_map<std::string, sf::Image> imageCache;
+    static std::unordered_map<std::string, sf::Texture> textureCache;
+    static std::unordered_map<std::string, sf::Font> fontCache;
+    static std::unordered_map<std::string, sf::SoundBuffer> soundBufferCache;
+    static std::unordered_map<std::string, sf::Music> musicCache;
+
 public:
-	template<typename T> static void LoadFormFile(T& t, const std::string& fileName);
-	static bool getMissingFileList(std::string& fileList);
+    // Delete copy constructor and assignment operator to prevent duplication
+    FileManager(const FileManager&) = delete;
+    FileManager& operator=(const FileManager&) = delete;
 
-	inline static bool IsAnyFileMissing();
+    // Template function to load a resource from a file
+    template<typename T> 
+    static void LoadFromFile(T& resource, const std::string& fileName);
+
+    // Function to get a list of missing files
+    static bool getMissingFileList(std::string& fileList);
+
+    // Function to check if any file is missing
+    inline static bool IsAnyFileMissing();
+
+    // Function to log a loading failure with the filename
+    static void LogLoadingFailure(const std::string& fileName);
 };
-
