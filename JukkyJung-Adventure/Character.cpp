75df273f669e0fadb1e71633ca49cb0e2b4efac7
Pasitha <pasithacppsom@hotmail.com>
2023-11-17 22:42:27 +0700

    Updatthe constructor and add sf variable

--- JukkyJung-Adventure/Character.cpp
+++ JukkyJung-Adventure/Character.cpp
@@ -1,10 +1,22 @@
 #include "common.h"
-
 #include "Character.h"
 
-// Constructor implementation
+const float Character::JUKKYJUNG_SCALE = 1.0f; // Adjust the scale as needed
+const sf::Vector2f Character::JUKKYJUNG_POSITION = { 0.0f, 0.0f }; // Adjust the position as needed
+
 Character::Character(const std::string& name, int health, int baseAttack, ElementalPower elementalPower)
-    : name(name), health(health), baseAttack(baseAttack), elementalPower(elementalPower) {}
+    : m_windowInstance(nullptr), name(name), health(health), baseAttack(baseAttack), elementalPower(elementalPower) {
+    FileManager::LoadFromFile(this->m_TextFont, "asset/font/ReadexPro.ttf");
+    FileManager::LoadFromFile(this->m_JukkyJungTexture, "asset/picture/JukkyJung.png");
+
+    m_JukkyJungHp.setFillColor(sf::Color::Black);
+    m_JukkyJungHp.setFont(this->m_TextFont);
+    m_JukkyJungHp.setString("HP: 100");
+
+    this->m_JukkyJungSprite.setTexture(this->m_JukkyJungTexture);
+    this->m_JukkyJungSprite.setScale({ this->JUKKYJUNG_SCALE, this->JUKKYJUNG_SCALE });
+    this->m_JukkyJungSprite.setPosition(this->JUKKYJUNG_POSITION);
+}
 
 // Getter for elemental power
 ElementalPower Character::getElementalPower() const {
@@ -14,4 +26,9 @@ ElementalPower Character::getElementalPower() const {
 // Getter for base attack
 int Character::getBaseAttack() const {
     return baseAttack;
+}
+
+// Getter for character name
+const std::string& Character::getName() const {
+    return name;
 }
--- JukkyJung-Adventure/Character.h
+++ JukkyJung-Adventure/Character.h
@@ -19,12 +19,6 @@ enum class ElementalPower {
 };
 
 class Character {
-private:
-    std::string name;
-    int health;
-    int baseAttack;
-    ElementalPower elementalPower;
-
 public:
     // Constructor with elemental power and base attack parameters
     Character(const std::string& name, int health, int baseAttack, ElementalPower elementalPower);
@@ -32,6 +26,24 @@ public:
     // Getter for elemental power
     ElementalPower getElementalPower() const;
 
+    // Getter for character name
+    const std::string& getName() const;
+
     // Getter for base attack
     int getBaseAttack() const;
+private:
+	sf::RenderWindow* m_windowInstance;
+    std::string name;
+    int health;
+	sf::Font m_TextFont;
+	sf::Text m_JukkyJungHp;
+    int baseAttack;
+    ElementalPower elementalPower;
+
+	static const float JUKKYJUNG_SCALE;
+	static const sf::Vector2f JUKKYJUNG_POSITION;
+
+	sf::Texture m_JukkyJungTexture;
+	sf::Sprite m_JukkyJungSprite;
+
 };
