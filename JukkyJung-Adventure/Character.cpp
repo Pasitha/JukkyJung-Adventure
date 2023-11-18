6c4e3e9697e544f6682ebced3734a359ae5b010c
Pasitha <pasithacppsom@hotmail.com>
2023-11-18 23:06:58 +0700

    Update Character Class

--- JukkyJung-Adventure/Character.cpp
+++ JukkyJung-Adventure/Character.cpp
@@ -5,13 +5,13 @@ const float Character::JUKKYJUNG_SCALE = 1.0f; // Adjust the scale as needed
 const sf::Vector2f Character::JUKKYJUNG_POSITION = { 0.0f, 0.0f }; // Adjust the position as needed
 
 Character::Character(const std::string& name, int health, int baseAttack, ElementalPower elementalPower)
-    : m_windowInstance(nullptr), name(name), health(health), baseAttack(baseAttack), elementalPower(elementalPower) {
+    : name(name), health(health), baseAttack(baseAttack), elementalPower(elementalPower) {
     FileManager::LoadFromFile(this->m_TextFont, "asset/font/ReadexPro.ttf");
     FileManager::LoadFromFile(this->m_JukkyJungTexture, "asset/picture/JukkyJung.png");
 
-    m_JukkyJungHp.setFillColor(sf::Color::Black);
-    m_JukkyJungHp.setFont(this->m_TextFont);
-    m_JukkyJungHp.setString("HP: 100");
+    this->m_JukkyJungHp.setFillColor(sf::Color::Black);
+    this->m_JukkyJungHp.setFont(this->m_TextFont);
+    this->m_JukkyJungHp.setString("HP: 100");
 
     this->m_JukkyJungSprite.setTexture(this->m_JukkyJungTexture);
     this->m_JukkyJungSprite.setScale({ this->JUKKYJUNG_SCALE, this->JUKKYJUNG_SCALE });
@@ -23,12 +23,35 @@ ElementalPower Character::getElementalPower() const {
     return elementalPower;
 }
 
+// Getter for character name
+const std::string& Character::getName() const {
+    return name;
+}
+
+// Getter for current health
+int Character::getHealth() const {
+    return health;
+}
+
 // Getter for base attack
 int Character::getBaseAttack() const {
     return baseAttack;
 }
 
-// Getter for character name
-const std::string& Character::getName() const {
-    return name;
+// Check if the character is defeated
+bool Character::isDefeated() const {
+    return health <= 0;
+}
+
+// Function to reduce health by a specified amount
+void Character::takeDamage(int damage) {
+    health -= damage;
+    if (health < 0) {
+        health = 0;
+    }
+}
+
+// Function to draw the character on the game window
+void Character::draw(sf::RenderWindow* window) {
+    window->draw(this->m_JukkyJungSprite);
 }
--- JukkyJung-Adventure/Character.h
+++ JukkyJung-Adventure/Character.h
@@ -25,25 +25,35 @@ public:
 
     // Getter for elemental power
     ElementalPower getElementalPower() const;
-
     // Getter for character name
     const std::string& getName() const;
-
+    // Getter for current health
+    int getHealth() const;
     // Getter for base attack
     int getBaseAttack() const;
+    // Check if the character is defeated (health <= 0)
+    bool isDefeated() const;
+    // Function to reduce health by a specified amount
+    void takeDamage(int damage);
+    // Function to draw the character on the game window
+    void draw(sf::RenderWindow* window);
+
 private:
-	sf::RenderWindow* m_windowInstance;
     std::string name;
     int health;
-	sf::Font m_TextFont;
-	sf::Text m_JukkyJungHp;
     int baseAttack;
     ElementalPower elementalPower;
 
-	static const float JUKKYJUNG_SCALE;
-	static const sf::Vector2f JUKKYJUNG_POSITION;
+    // Constants for positioning and scaling of the character sprite
+    static const float JUKKYJUNG_SCALE;
+    static const sf::Vector2f JUKKYJUNG_POSITION;
 
-	sf::Texture m_JukkyJungTexture;
-	sf::Sprite m_JukkyJungSprite;
+    // Texture and sprite for the character
+    sf::Texture m_JukkyJungTexture;
+    sf::Sprite m_JukkyJungSprite;
 
+    // Font and text for displaying health
+    sf::Font m_TextFont;
+    sf::Text m_JukkyJungHp;
 };
+
