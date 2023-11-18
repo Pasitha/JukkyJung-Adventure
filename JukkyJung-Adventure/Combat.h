ec59c0ccfc1eb8606a3721121777ad0164d65a85
Pasitha <pasithacppsom@hotmail.com>
2023-11-18 23:07:21 +0700

    Add some method to combat system

--- JukkyJung-Adventure/Combat.h
+++ JukkyJung-Adventure/Combat.h
@@ -1,7 +1,18 @@
 #pragma once
 #include "common.h"
 
-class Combat
-{
-};
+class Combat {
+public:
+    Combat(const std::vector<Character>& team1, const std::vector<Character>& team2)
+        : team1(team1), team2(team2) {}
 
+    void StartCombat();
+
+private:
+    std::vector<Character> team1;
+    std::vector<Character> team2;
+
+    bool IsCombatOngoing() const;
+    void PerformRound();
+    Character& ChooseTarget(std::vector<Character>& targetTeam);
+};
