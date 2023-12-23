e2a0be4f9aba8e99cc21776ebca08104f19969fa
Pasitha <pasithacppsom@hotmail.com>
2023-12-24 00:51:58 +0700

    Moved the forward declarations of classes to the top for better organization

--- JukkyJung-Adventure/Game.h
+++ JukkyJung-Adventure/Game.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "common.h"
 
 // Forward declarations for classes used in Game
@@ -10,48 +11,47 @@ class Character;
 // Game class is responsible for managing the game loop, handling events, and rendering scenes.
 class Game {
 private:
-	// Enumeration representing different scenes in the game
-	enum class Scene {
-		mainMenu,
-		setting,
-		gamePlay,
-		pauseMenu
-	};
+    // Enumeration representing different scenes in the game
+    enum class Scene {
+        MainMenu,    // Main menu scene
+        Setting,     // Setting scene (not yet implemented)
+        GamePlay,    // Game play scene
+        PauseMenu    // Pause menu scene
+    };
 
-	// Struct representing game components for each scene such that Button, JukkyJug character and Enemy character component
-	struct m_gameComponent {
-		std::unique_ptr<Button> m_Button;
-		std::unique_ptr<JukkyJung> m_JukkyJung;
-		std::unique_ptr<Enemy> m_Enemy;
-		std::unique_ptr<Character> m_Character;
-	};
+    // Struct representing game components for each scene such that Button, JukkyJug character, and Enemy character component
+    struct SceneComponents {
+        std::unique_ptr<Button> button;
+        std::unique_ptr<JukkyJung> jukkyJung;
+        std::unique_ptr<Enemy> enemy;
+        std::unique_ptr<Character> character;
+    };
 
 public:
-	Game();
-	// Add a destructor only in debug mode
+    // Constructor to initialize the game
+    Game();
+
+    // Destructor added only in debug mode as a reminder for cleanup
 #ifdef _DEBUG
-	~Game();
+    ~Game();
 #endif
 
-	// Function to handle user input events
-	void HandleEvents();
-	// Function to render the current state of the game
-	void Render();
-	// Main game loop function
-	void GameLoop();
+    // Function to handle user input events
+    void HandleEvents();
+    // Function to render the current state of the game
+    void Render();
+    // Main game loop function
+    void GameLoop();
 
 private:
-	sf::RenderWindow m_window;                  // SFML window for rendering
-
-    Scene m_gameScene;                          // Current scene in the game
-    std::unordered_map<Scene, std::unique_ptr<m_gameComponent>> m_SceneComponent;  // Map of scenes to their components
-
-    sf::Font m_gameFont;                        // Font used in the game for text rendering
-
-    bool m_isGamePause;                         // Flag indicating whether the game is in a paused state
-    sf::Text m_backgroundPauseMenuText;         // Text displayed in the background during pause
-    sf::RectangleShape m_backgroundPauseMenu;   // Background shape during pause
+    sf::RenderWindow window;                  // SFML window for rendering
+    Scene currentScene;                       // Current scene in the game
+    std::unordered_map<Scene, std::unique_ptr<SceneComponents>> sceneComponents;  // Map of scenes to their components
+    sf::Font gameFont;                        // Font used in the game for text rendering
+    bool isGamePaused;                        // Flag indicating whether the game is in a paused state
+    sf::Text backgroundPauseMenuText;         // Text displayed in the background during pause
+    sf::RectangleShape backgroundPauseMenu;   // Background shape during pause
 
     // Function to initialize components for a specific scene with buttons and positions
-    void initializeSceneComponent(Scene scene, const std::vector<std::pair<std::string, sf::Vector2f>>& buttons);
+    void InitializeSceneComponents(Scene scene, const std::vector<std::pair<std::string, sf::Vector2f>>& buttons);
 };
