6277d4de43f9af86ae754e4b122cd8adfbd8c2d0
Pasitha <pasithacppsom@hotmail.com>
2023-12-24 02:18:39 +0700

    add LoadAsync method

--- JukkyJung-Adventure/FileManager.h
+++ JukkyJung-Adventure/FileManager.h
@@ -23,6 +23,10 @@ private:
     static std::unordered_map<std::string, sf::SoundBuffer> soundBufferCache;
     static std::unordered_map<std::string, sf::Music> musicCache;
 
+    // Helper function for asynchronous loading
+    template<typename T>
+    static void LoadAsync(T& resource, const std::string& fileName, std::promise<bool>& promise);
+
 public:
     // Delete copy constructor and assignment operator to prevent duplication
     FileManager(const FileManager&) = delete;
@@ -31,6 +35,28 @@ public:
     // Template function to load a resource from a file
     template<typename T> 
     static bool LoadFromFile(T& resource, const std::string& fileName);
+
+    // Template function to load a resource from a file asynchronously
+    template<typename T>
+    static std::future<bool> LoadAsync(const std::string& fileName);
+
+    // Function to clear the entire cache
+    static void ClearCache();
+
+    // Function to clear image cache
+    static void ClearImageCache();
+
+    // Function to clear texture cache
+    static void ClearTextureCache();
+
+    // Function to clear font cache
+    static void ClearFontCache();
+
+    // Function to clear sound buffer cache
+    static void ClearSoundBufferCache();
+
+    // Function to clear music cache
+    static void ClearMusicCache();
 
     // Function to get a list of missing files
     static bool getMissingFileList(std::string& fileList);
