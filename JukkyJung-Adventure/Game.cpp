da12755cdfa2c4de325e77a5bb5b5ba4a8491b67
Pasitha <pasithacppsom@hotmail.com>
2023-12-24 00:52:13 +0700

    Moved the forward declarations of classes to the top for better organization

--- JukkyJung-Adventure/Game.cpp
+++ JukkyJung-Adventure/Game.cpp
@@ -2,127 +2,129 @@
 
 // Constructor to initialize the game
 Game::Game() :
-    m_window(sf::VideoMode(1920, 1080), "JukkyJuung Adventure", sf::Style::Fullscreen), 
-    m_gameScene(Scene::mainMenu),
-    m_isGamePause(false)
+    window(sf::VideoMode(1920, 1080), "JukkyJuung Adventure", sf::Style::Fullscreen),
+    currentScene(Scene::MainMenu),
+    isGamePaused(false)
 {
     // Set a frame rate limit to the window
-    this->m_window.setFramerateLimit(60);
+    window.setFramerateLimit(60);
 
     // Load the game font from a file
-    FileManager::LoadFromFile(this->m_gameFont, "asset/font/ReadexPro.ttf");
+    FileManager::LoadFromFile(gameFont, "asset/font/ReadexPro.ttf");
 
     // Initialize variables related to the game pause menu
-    this->m_backgroundPauseMenuText.setString("Pause");
-    this->m_backgroundPauseMenuText.setFont(this->m_gameFont);
-    this->m_backgroundPauseMenuText.setPosition({ 960, 100 });
-    this->m_backgroundPauseMenu.setSize({ 1920.f, 1080.f });
-    this->m_backgroundPauseMenu.setFillColor(sf::Color(0, 0, 0, 155));
+    backgroundPauseMenuText.setString("Pause");
+    backgroundPauseMenuText.setFont(gameFont);
+    backgroundPauseMenuText.setPosition({ 960, 100 });
+
+    backgroundPauseMenu.setSize({ 1920.f, 1080.f });
+    backgroundPauseMenu.setFillColor(sf::Color(0, 0, 0, 155));
 
     // Initialize game components for each scene
-    this->m_SceneComponent[Scene::mainMenu] = std::make_unique<m_gameComponent>(); 
-    this->m_SceneComponent[Scene::gamePlay] = std::make_unique<m_gameComponent>(); 
-    this->m_SceneComponent[Scene::pauseMenu] = std::make_unique<m_gameComponent>();
+    sceneComponents[Scene::MainMenu] = std::make_unique<SceneComponents>();
+    sceneComponents[Scene::GamePlay] = std::make_unique<SceneComponents>();
+    sceneComponents[Scene::PauseMenu] = std::make_unique<SceneComponents>();
 
     // Create and associate buttons with each scene
-    this->m_SceneComponent[Scene::mainMenu]->m_Button = std::make_unique<Button>(&this->m_window);
-    this->m_SceneComponent[Scene::gamePlay]->m_Button = std::make_unique<Button>(&this->m_window);
-    this->m_SceneComponent[Scene::gamePlay]->m_JukkyJung = std::make_unique<JukkyJung>(&this->m_window);
-    this->m_SceneComponent[Scene::gamePlay]->m_Enemy = std::make_unique<Enemy>(&this->m_window);
-    this->m_SceneComponent[Scene::gamePlay]->m_Character = std::make_unique<Character>();
-    this->m_SceneComponent[Scene::pauseMenu]->m_Button = std::make_unique<Button>(&this->m_window);
-    
-    // Initialize scene components with buttons and their positions
-    initializeSceneComponent(Scene::mainMenu, { {"Play", {50, 300}}, {"Setting", {50, 500}}, {"Exit", {50, 700}} });
-    initializeSceneComponent(Scene::gamePlay, { {"ATTACK", {50, 800}}, {"ITEM", {400, 800}}, {"SKIP ROUND", {750, 800}} });
-    initializeSceneComponent(Scene::pauseMenu, { {"RESUME", {850, 300}}, {"EXIT", {850, 500}} });
+    sceneComponents[Scene::MainMenu]->button = std::make_unique<Button>(&window);
+    sceneComponents[Scene::GamePlay]->button = std::make_unique<Button>(&window);
+    sceneComponents[Scene::GamePlay]->jukkyJung = std::make_unique<JukkyJung>(&window);
+    sceneComponents[Scene::GamePlay]->enemy = std::make_unique<Enemy>(&window);
+    sceneComponents[Scene::GamePlay]->character = std::make_unique<Character>();
+    sceneComponents[Scene::PauseMenu]->button = std::make_unique<Button>(&window);
 
+    // Initialize scene components with buttons and their positions
+    InitializeSceneComponents(Scene::MainMenu, { {"Play", {50, 300}}, {"Setting", {50, 500}}, {"Exit", {50, 700}} });
+    InitializeSceneComponents(Scene::GamePlay, { {"ATTACK", {50, 800}}, {"ITEM", {400, 800}}, {"SKIP ROUND", {750, 800}} });
+    InitializeSceneComponents(Scene::PauseMenu, { {"RESUME", {850, 300}}, {"EXIT", {850, 500}} });
 }
 
-// Destructor implementation only in debug mode
 #ifdef _DEBUG
+// Destructor implementation only in debug mode as a reminder for cleanup
 Game::~Game() {
-    std::cout << "Game was destory!" << std::endl;
+    std::cout << "Game was destroyed!" << std::endl;
 }
 #endif
 
 // Initialize scene components with buttons and their positions
-void Game::initializeSceneComponent(Scene scene, const std::vector<std::pair<std::string, sf::Vector2f>>& buttons) {
-    auto& sceneComp = *this->m_SceneComponent[scene];
+void Game::InitializeSceneComponents(Scene scene, const std::vector<std::pair<std::string, sf::Vector2f>>& buttons) {
+    auto& sceneComp = *sceneComponents[scene];
 
-    sceneComp.m_Button = std::make_unique<Button>(&this->m_window);
+    sceneComp.button = std::make_unique<Button>(&window);
 
     // Add buttons to the scene component
     for (const auto& button : buttons) {
-        sceneComp.m_Button->addButton(button.first, button.second);
+        sceneComp.button->addButton(button.first, button.second);
     }
 }
 
 // Handle events based on the current game scene
 void Game::HandleEvents() {
     sf::Event event;
-    while (this->m_window.pollEvent(event)) {
+    while (window.pollEvent(event)) {
         if (event.type == sf::Event::Closed) {
-            this->m_window.close();
+            window.close();
         }
 
         // The event check handles each game scene separately.
-        switch (m_gameScene) {
-        case Scene::mainMenu:
+        switch (currentScene) {
+        case Scene::MainMenu:
             // Handle events for the main menu scene
             if (event.type == sf::Event::MouseMoved) {
-                this->m_SceneComponent[Scene::mainMenu]->m_Button->isHover();
+                sceneComponents[Scene::MainMenu]->button->isHover();
             }
             if (event.type == sf::Event::MouseButtonPressed) {
-                switch (this->m_SceneComponent[Scene::mainMenu]->m_Button->whichButtonHover()) {
+                switch (sceneComponents[Scene::MainMenu]->button->whichButtonHover()) {
                 case 0:
-                    this->m_gameScene = Scene::gamePlay;
+                    currentScene = Scene::GamePlay;
                     break;
                 case 1:
-                    this->m_gameScene = Scene::setting;
+                    currentScene = Scene::Setting;
                     break;
                 case 2:
-                    this->m_window.close();
+                    window.close();
                     break;
                 }
             }
             break;
-        case Scene::gamePlay:
+        case Scene::GamePlay:
             // Handle events for the game play scene
             if (event.type == sf::Event::MouseMoved) {
-                this->m_SceneComponent[Scene::gamePlay]->m_Button->isHover();
-                if (this->m_isGamePause) {
-                    this->m_SceneComponent[Scene::pauseMenu]->m_Button->isHover();
+                sceneComponents[Scene::GamePlay]->button->isHover();
+                if (isGamePaused) {
+                    sceneComponents[Scene::PauseMenu]->button->isHover();
                 }
             }
             if (event.type == sf::Event::MouseButtonPressed) {
-                switch (this->m_SceneComponent[Scene::gamePlay]->m_Button->whichButtonHover()) {
+                switch (sceneComponents[Scene::GamePlay]->button->whichButtonHover()) {
                 case 0:
-                    this->m_SceneComponent[Scene::gamePlay]->m_JukkyJung->ShakeAnimation();
+                    sceneComponents[Scene::GamePlay]->jukkyJung->ShakeAnimation();
                     break;
                 case 1:
+                    // Handle ITEM button press
                     break;
                 case 2:
+                    // Handle SKIP ROUND button press
                     break;
                 }
 
-                if (this->m_isGamePause) {
-                    switch (this->m_SceneComponent[Scene::pauseMenu]->m_Button->whichButtonHover()) {
+                if (isGamePaused) {
+                    switch (sceneComponents[Scene::PauseMenu]->button->whichButtonHover()) {
                     case 0:
-                        this->m_isGamePause = false;
+                        isGamePaused = false;
                         break;
                     case 1:
-                        this->m_gameScene = Scene::mainMenu;
+                        currentScene = Scene::MainMenu;
                         break;
                     }
                 }
             }
             if (event.type == sf::Event::KeyPressed) {
-                if (event.key.code == sf::Keyboard::Escape && !this->m_isGamePause) {
-                    this->m_isGamePause = true;
+                if (event.key.code == sf::Keyboard::Escape && !isGamePaused) {
+                    isGamePaused = true;
                 }
-                else if (event.key.code == sf::Keyboard::Escape && this->m_isGamePause) {
-                    this->m_isGamePause = false;
+                else if (event.key.code == sf::Keyboard::Escape && isGamePaused) {
+                    isGamePaused = false;
                 }
             }
             break;
@@ -132,36 +134,37 @@ void Game::HandleEvents() {
 
 // Render the current game scene
 void Game::Render() {
-    this->m_window.clear(sf::Color(199, 119, 19, 255));
-    switch (m_gameScene) {
-        case Scene::mainMenu:
-            // Render the main menu scene
-            this->m_SceneComponent[Scene::mainMenu]->m_Button->Update();
-            break;
-        case Scene::setting:
-            // Render the settings scene
-            break;
-        case Scene::gamePlay:
-            // Render the game play scene
-            this->m_SceneComponent[Scene::gamePlay]->m_Button->Update();
-            this->m_SceneComponent[Scene::gamePlay]->m_JukkyJung->Update();
-            this->m_SceneComponent[Scene::gamePlay]->m_Enemy->Update();
+    window.clear(sf::Color(199, 119, 19, 255));
 
-            if (this->m_isGamePause) {
-                // Render the pause menu when the game is paused
-                this->m_window.draw(m_backgroundPauseMenu);
-                this->m_window.draw(m_backgroundPauseMenuText);
+    switch (currentScene) {
+    case Scene::MainMenu:
+        // Render the main menu scene
+        sceneComponents[Scene::MainMenu]->button->Update();
+        break;
+    case Scene::Setting:
+        // Render the settings scene (not yet implemented)
+        break;
+    case Scene::GamePlay:
+        // Render the game play scene
+        sceneComponents[Scene::GamePlay]->button->Update();
+        sceneComponents[Scene::GamePlay]->jukkyJung->Update();
+        sceneComponents[Scene::GamePlay]->enemy->Update();
 
-                this->m_SceneComponent[Scene::pauseMenu]->m_Button->Update();
-            }
-            break;
+        if (isGamePaused) {
+            // Render the pause menu when the game is paused
+            window.draw(backgroundPauseMenu);
+            window.draw(backgroundPauseMenuText);
+
+            sceneComponents[Scene::PauseMenu]->button->Update();
+        }
+        break;
     }
-    this->m_window.display();
+    window.display();
 }
 
 // Run the game loop
 void Game::GameLoop() {
-    while (this->m_window.isOpen()) {
+    while (window.isOpen()) {
         HandleEvents();
         Render();
     }
