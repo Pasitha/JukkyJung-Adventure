30c696c52a8b11ff0d457ca1b15e6e13a0f20777
Pasitha <pasithacppsom@hotmail.com>
2023-12-23 23:35:29 +0700

    Clear old Combat Class

--- JukkyJung-Adventure/Combat.h
+++ JukkyJung-Adventure/Combat.h
@@ -6,13 +6,8 @@ public:
     Combat(const std::vector<Character>& team1, const std::vector<Character>& team2)
         : team1(team1), team2(team2) {}
 
-    void StartCombat();
 
 private:
     std::vector<Character> team1;
     std::vector<Character> team2;
-
-    bool IsCombatOngoing() const;
-    void PerformRound();
-    Character& ChooseTarget(std::vector<Character>& targetTeam);
 };
