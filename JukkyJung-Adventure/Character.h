c63dd20fff8dcf59373b46d76139aa0a0324b1c0
Pasitha <pasithacppsom@hotmail.com>
2023-12-23 21:24:26 +0700

    add Character management system to Character Class

--- JukkyJung-Adventure/Character.h
+++ JukkyJung-Adventure/Character.h
@@ -18,42 +18,73 @@ enum class ElementalPower {
     Gravity
 };
 
+// Class to represent a character in the game
 class Character {
 public:
-    // Constructor with elemental power and base attack parameters
-    Character(const std::string& name, int health, int baseAttack, ElementalPower elementalPower);
+    // Default constructor
+    Character();
 
     // Getter for elemental power
-    ElementalPower getElementalPower() const;
-    // Getter for character name
-    const std::string& getName() const;
-    // Getter for current health
-    int getHealth() const;
-    // Getter for base attack
-    int getBaseAttack() const;
-    // Check if the character is defeated (health <= 0)
-    bool isDefeated() const;
-    // Function to reduce health by a specified amount
-    void takeDamage(int damage);
+    ElementalPower getElementalPower(std::string name) const;
+
+    // Getter methods for individual attributes
+    std::string getName(std::string characterName) const;
+    int getHealth(std::string characterName) const;
+    int getAttack(std::string characterName) const;
+    ElementalPower getElementalPower(std::string characterName) const;
+
+	// Setter methods for individual attributes
+	void Character::setName(std::string characterName, const std::string& newName) {
+		getCharacterAttributes(characterName).name = newName;
+	}
+	void Character::setHealth(std::string characterName, int newHealth) {
+		getCharacterAttributes(characterName).health = newHealth;
+	}
+	void Character::setAttack(std::string characterName, int newAttack) {
+		getCharacterAttributes(characterName).attack = newAttack;
+	}
+	void Character::setElementalPower(std::string characterName, ElementalPower newElementalPower) {
+		getCharacterAttributes(characterName).elementalPower = newElementalPower;
+	}
+
     // Function to draw the character on the game window
-    void draw(sf::RenderWindow* window);
+    // Parameters:
+    // - window: Pointer to the game window
+    // - name: Name of the character to be drawn
+    void draw(sf::RenderWindow* window, std::string name);
 
 private:
-    std::string name;
-    int health;
-    int baseAttack;
-    ElementalPower elementalPower;
+    // Struct to represent attributes of the character
+    struct CharacterAttributes {
+        // Name of the character
+        std::string name;
+        // Health points of the character
+        int health;
+        // Base attack points of the character
+        int attack;
+        // Elemental power of the character
+        ElementalPower elementalPower;
 
-    // Constants for positioning and scaling of the character sprite
-    static const float JUKKYJUNG_SCALE;
-    static const sf::Vector2f JUKKYJUNG_POSITION;
+        // Texture and sprite for character's visual representation
+        sf::Texture texture;
+        sf::Sprite sprite;
 
-    // Texture and sprite for the character
-    sf::Texture m_JukkyJungTexture;
-    sf::Sprite m_JukkyJungSprite;
+        // Font and text for displaying health
+        sf::Text healthText;
+    };
 
-    // Font and text for displaying health
-    sf::Font m_TextFont;
-    sf::Text m_JukkyJungHp;
+    // Static constant for scaling factor of the character
+    static const float Scale;
+
+    // Static font for text rendering
+    static sf::Font textFont;
+
+    // Unordered map to store attributes of different characters
+    std::unordered_map<std::string, CharacterAttributes> charactersAttributes;
+
+    // Private helper function to get attributes
+    const CharacterAttributes& getCharacterAttributes(std::string name) const;
+
+    // Private helper function to add a character
+    void addCharacter(const std::string& name, int health, int attack, ElementalPower elementalPower, const std::string& characterTexturePath);
 };
-
