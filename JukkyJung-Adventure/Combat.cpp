dc999ca8f980e8d4d407a7ebfe042d9c4292920c
Pasitha <pasithacppsom@hotmail.com>
2024-01-01 23:52:58 +0700

    Add log into method PerformAttack

--- JukkyJung-Adventure/Combat.cpp
+++ JukkyJung-Adventure/Combat.cpp
@@ -1,7 +1,12 @@
 #include "Combat.h"
 
+Combat::Combat(const std::vector<Character>& team1, const std::vector<Character>& team2) : team1(team1), team2(team2) {
+
+}
+
 // Function to simulate a simple attack
-void PerformAttack(Character& attacker, Character& target) {
+void Combat::PerformAttack(Character& attacker, Character& target) {
 #ifdef _DEBUG
+	std::cout << "Start Combat!" << std::endl;
 #endif
 }
