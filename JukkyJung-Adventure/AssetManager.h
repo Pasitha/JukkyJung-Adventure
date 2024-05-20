13aca1f8a2e42bf596e7c944683bf7984c103b19
Pasitha <pasithacppsom@hotmail.com>
2024-05-20 23:52:05 +0700

    change unordered_map name

--- JukkyJung-Adventure/AssetManager.h
+++ JukkyJung-Adventure/AssetManager.h
@@ -8,15 +8,15 @@ public:
     AssetType& Get(const std::string& filename);
 
 private:
-    std::unordered_map<std::string, std::unique_ptr<AssetType>> assets_;
+    std::unordered_map<std::string, std::unique_ptr<AssetType>> assetPool;
 };
 
 template <>
 class AssetManager<SpriteAnimation> {
 public:
-    SpriteAnimation* Load(const std::string& filename, const std::vector<sf::IntRect>& frames, float frameDuration);
+    SpriteAnimation* Load(const std::string& filename, sf::Vector2i frameSize, int rowCount);
     SpriteAnimation& Get(const std::string& filename);
 
 private:
-    std::unordered_map<std::string, std::unique_ptr<SpriteAnimation>> assets_;
+    std::unordered_map<std::string, std::unique_ptr<SpriteAnimation>> assetPool;
 };
