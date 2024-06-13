0884b8bb57fde20fe4f9a563a12978ce45693c2f
Pasitha <pasithacppsom@hotmail.com>
2024-06-13 17:56:30 +0700

    remove setTile

--- JukkyJung-Adventure/MapManager.cpp
+++ JukkyJung-Adventure/MapManager.cpp
@@ -23,27 +23,6 @@ void MapManager::addMap(const std::string& name, int tileWidth, int tileHeight, 
     m_maps[name] = map;
 }
 
-// Sets the tile type at the specified position on the map
-void MapManager::setTile(const std::string& name, int x, int y, int tileType) {
-    auto it = m_maps.find(name);
-    if (it != m_maps.end()) {
-        if (x >= 0 && x < it->second.width && y >= 0 && y < it->second.height) {
-			it->second.tiles[y * it->second.width + x] = tileType;
-
-			// Create a sprite for this tile
-			sf::Sprite sprite;
-			sprite.setTexture(it->second.tilesetTexture);
-			sprite.setTextureRect(sf::IntRect(tileType * it->second.tileSize, 0, it->second.tileSize, it->second.tileSize));
-			sprite.setPosition(x * it->second.tileSize, y * it->second.tileSize);
-			it->second.tileSprites.push_back(sprite);
-		}
-    }
-}
-
-// Sets the tile type at the specified position on the map
-void MapManager::setTile(Map& map, int x, int y, int tileType) {
-    }
-
 // Draws all maps managed by the MapManager
 void MapManager::draw() {
     for (auto& pair : m_maps) {
