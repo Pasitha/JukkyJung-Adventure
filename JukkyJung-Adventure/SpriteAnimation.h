0055ed151b0ee0ea88815187e10eede3f5ba1115
Pasitha <pasithacppsom@hotmail.com>
2024-06-17 02:11:26 +0700

    change int to uint64_t

--- JukkyJung-Adventure/SpriteAnimation.h
+++ JukkyJung-Adventure/SpriteAnimation.h
@@ -4,61 +4,104 @@
 // SpriteAnimation class is responsible for managing and displaying sprite animations
 class SpriteAnimation {
 public:
-    // Constructor taking a pointer to the SFML render window
+    // Constructor to initialize the SpriteAnimation with a pointer to the SFML render window
     SpriteAnimation(sf::RenderWindow* window);
 
-    // Load animation frames from a sprite sheet with multiple rows and columns
-    void loadSpriteSheet(const std::string& filePath, const std::string& spriteName, sf::Vector2i frameSize, int rowCount, sf::Vector2f position = { 0, 0 });
+    /**
+     * Loads animation frames from a sprite sheet with multiple rows and columns.
+     * @param filePath Path to the sprite sheet image
+     * @param spriteName Name of the sprite
+     * @param frameSize Size of each frame in the sprite sheet
+     * @param rowCount Number of rows in the sprite sheet
+     * @param position Initial position of the sprite (default: {0, 0})
+     */
+    void loadSpriteSheet(const std::string& filePath, const std::string& spriteName, sf::Vector2i frameSize, uint64_t rowCount, sf::Vector2f position = {0, 0});
 
-    // Set the scale of the sprite
+    /**
+     * Sets the scale of the sprite.
+     * @param spriteName Name of the sprite
+     * @param scale Scale vector (x, y) for the sprite
+     */
     void setScale(const std::string& spriteName, const sf::Vector2f& scale);
 
-    // Set the current state (row) of the animation
-    void setState(const std::string& spriteName, const std::string& stateName, int startRow, int frameCount, float duration);
-    void setState(const std::string& spriteName, const std::string& stateName, int startRow, int startCol, int endCol, float duration);
+    /**
+     * Sets the current state (row) of the animation.
+     * @param spriteName Name of the sprite
+     * @param stateName Name of the animation state
+     * @param startRow Row index of the animation in the sprite sheet
+     * @param frameCount Number of frames in the animation
+     * @param duration Duration of each frame
+     */
+    void setState(const std::string& spriteName, const std::string& stateName, uint64_t startRow, uint64_t frameCount, float duration);
 
-    // Change to a different state
+    /**
+     * Sets the current state (row and column range) of the animation.
+     * @param spriteName Name of the sprite
+     * @param stateName Name of the animation state
+     * @param startRow Row index of the animation in the sprite sheet
+     * @param startCol Starting column index of the animation
+     * @param endCol Ending column index of the animation
+     * @param duration Duration of each frame
+     */
+    void setState(const std::string& spriteName, const std::string& stateName, uint64_t startRow, uint64_t startCol, uint64_t endCol, float duration);
+
+    /**
+     * Changes to a different state.
+     * @param spriteName Name of the sprite
+     * @param stateName Name of the new state
+     * @param resetCurrentFrame Flag to reset the current frame to the first frame of the new state (default: false)
+     */
     void changeState(const std::string& spriteName, const std::string& stateName, bool resetCurrentFrame = false);
 
-    // Move the sprite by a given offset
+    /**
+     * Moves the sprite by a given offset.
+     * @param spriteName Name of the sprite
+     * @param offset Offset vector (x, y) to move the sprite
+     */
     void moveSprite(const std::string& spriteName, sf::Vector2f offset);
 
-    // Update the animation frame based on elapsed time
+    /**
+     * Updates the animation frame based on elapsed time.
+     * @param spriteName Name of the sprite
+     * @param deltaTime Time elapsed since the last frame update
+     */
     void updateAnimation(const std::string& spriteName, float deltaTime);
 
-    // Draw the current frame
+    /**
+     * Draws the current frame of the animation.
+     * @param spriteName Name of the sprite
+     */
     void drawAnimation(const std::string& spriteName);
 
 private:
     // Structure to hold information about an animation state
     struct AnimationState {
         std::vector<sf::IntRect> frames;  // Frames of the animation
-        int currentFrame;                 // Current frame being displayed
+        uint64_t currentFrame;            // Current frame being displayed
         float duration;                   // Duration of each frame
         float elapsedTime;                // Time elapsed since the last frame change
 
         AnimationState() : currentFrame(0), duration(0), elapsedTime(0) {}
     };
 
+    // Structure to hold information about an animation sprite
     struct AnimationSprite {
-		sf::Texture texture;                                     // Texture of the sprite sheet
-		sf::Sprite sprite;                                       // Sprite to display the current frame
-		sf::Vector2i frameSize;                                  // Size of each frame
-		int rowCount;                                            // Number of rows in the sprite sheet
+        sf::Texture texture;                                     // Texture of the sprite sheet
+        sf::Sprite sprite;                                       // Sprite to display the current frame
+        sf::Vector2i frameSize;                                  // Size of each frame
+        uint64_t rowCount;                                       // Number of rows in the sprite sheet
 
-		std::unordered_map<std::string, AnimationState> states;  // Map of state names to animation states
-		AnimationState* currentState;                            // Pointer to the current animation state
+        std::unordered_map<std::string, AnimationState> states;  // Map of state names to animation states
+        AnimationState* currentState;                            // Pointer to the current animation state
 
         AnimationSprite() : rowCount(0), currentState(nullptr) {}
     };
 
-	// Pointer to the SFML render window
-    sf::RenderWindow* windowInstance;
+    sf::RenderWindow* windowInstance;  // Pointer to the SFML render window
 
-    std::unordered_map<std::string, std::shared_ptr<AnimationSprite>> animationSprite;
+    std::unordered_map<std::string, std::shared_ptr<AnimationSprite>> animationSprites;  // Map of sprite names to their AnimationSprite
 
 #ifdef _DEBUG
-    // Debug shape to visualize the sprite boundaries in debug mode
-    std::map<std::string, sf::RectangleShape> debugShape;
+    std::map<std::string, sf::RectangleShape> debugShape;  // Debug shape to visualize the sprite boundaries in debug mode
 #endif
 };
