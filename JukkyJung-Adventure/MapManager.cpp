ce5dad14969f8c2a44525deb63a1f76fc1185150
Pasitha <pasithacppsom@hotmail.com>
2024-07-18 17:41:01 +0700

    implement setCharecterToMap

--- JukkyJung-Adventure/MapManager.cpp
+++ JukkyJung-Adventure/MapManager.cpp
@@ -95,6 +95,10 @@ void MapManager::setTileMap(const std::string& mapName, int layerID, const std::
     }
 }
 
+void MapManager::setCharacterToMap(const std::string& mapName, const std::string& characterName, uint16_t coordinateX, uint16_t coordinateY) {
+    
+}
+
 // Draws the specified map managed by the MapManager
 void MapManager::draw(const std::string& mapName) {
     // Set the camera view to the window
